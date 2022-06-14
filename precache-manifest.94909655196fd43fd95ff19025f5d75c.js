0a3cdd6e6341f4a26c4f0a93118918ed35471c7a
dishant-codes <dishantaarak2696@gmail.com>
2022-06-14 12:33:57 +0530

    Updates

---
+++ precache-manifest.94909655196fd43fd95ff19025f5d75c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2ec95afe27912886d4",
+    "url": "/color-bracket/static/css/main.e1cfe789.chunk.css"
+  },
+  {
+    "revision": "1e2ec95afe27912886d4",
+    "url": "/color-bracket/static/js/main.7d43bc3f.chunk.js"
+  },
+  {
+    "revision": "0c7ce1d1401c40eaed29",
+    "url": "/color-bracket/static/js/runtime~main.646f3b09.js"
+  },
+  {
+    "revision": "49d544f9d76b8010cb7b",
+    "url": "/color-bracket/static/js/2.dc86361e.chunk.js"
+  },
+  {
+    "revision": "21fe75630c45a9654a57f40d06009d0a",
+    "url": "/color-bracket/index.html"
+  }
+];
